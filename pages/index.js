5b63ffa03b718c8a8d9ffbc1526b9770b77c709a
Nikita Andreev <nikita.andreev.work@gmail.com>
2021-06-21 17:49:45 +0800

    Got rid of unnecessary parts

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,12 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Шоурум женской одежды | Москва</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          {/* (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.) */}
         </p>
       </section>
     
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -43,7 +43,7 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   )
 }
